b0cbfaca79070c390131201efb3bf5164b4390cd
Wormwaker <61955556+noexcept2005@users.noreply.github.com>
2025-11-09 18:32:04 +0800

    DefaultSettings.dll

---
+++ DefaultSettings/framework.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+#define NOMINMAX
+// Windows 头文件
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <unordered_map>
+using namespace std;
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+#include "../XorString.h"
